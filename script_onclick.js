8ef834443f59c1570874139d6b752417f8530747
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-05-12 17:56:35 -0300

    first commit

---
+++ script_onclick.js
@@ -0,0 +1,9 @@
+let conteudo = document.getElementById('conteudo')
+
+function mostrar(){
+    document.getElementById('conteudo').innerHTML = `Notícas processadores ficam mais, carros,
+    a cada dia, por conta dos impostos `;
+}
+function esconder(){
+    document.getElementById('conteudo').innerHTML = ``;
+}
