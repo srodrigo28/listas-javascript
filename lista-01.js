65d4e722f6c00d6681cc51762210bb07c65125a5
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-05-14 07:23:25 -0300

    update-list-witch-map

---
+++ lista-01.js
@@ -0,0 +1,24 @@
+let contas = [
+    { "id": 01, "descrição": "Energia", "valor": 189, "vencimento": "20-02-2023", "status": false },
+    { "id": 02, "descrição": "escola",  "valor": 880, "vencimento": "20-02-2023", "status": false },
+    { "id": 03, "descrição": "carro", "valor": 1.600, "vencimento": "20-02-2023", "status": false},
+]
+
+/** Exemplo 1
+for (i=0; i< contas.length; i++){
+    console.log(i[0])
+}
+*/
+
+/** Exemplo 2
+contas.forEach(item => {
+    console.log(item)
+});
+
+*/
+
+/** Exemplo 3
+contas.map(item => {
+    console.log(item)
+})
+ */
